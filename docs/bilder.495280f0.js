f90e8275dd8201c06b0224b231708415f5d1af13
Pauline <pasp2300@student.miun.se>
2024-01-21 12:22:10 +0100

    Publicering till Netlify ist

--- docs/bilder.495280f0.js
+++
@@ -1,2 +0,0 @@
-let e=0;const l=()=>{let l=document.getElementById("bildspel");for(let n=0;n<l.children.length;n++)console.log(n),n===e?l.children[n].style.display="block":l.children[n].style.display="none";++e>=l.children.length&&(e=0)};setInterval(l,5e3),l();
-//# sourceMappingURL=bilder.495280f0.js.map
